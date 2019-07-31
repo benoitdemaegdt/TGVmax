60e3f9f6be189ee8ade08d54440fd2aa59e38ffd
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-07-31 22:20:00 +0200

    standardize columns names in database

--- scripts/db.sql
+++ scripts/db.sql
@@ -11,8 +11,8 @@ CREATE TABLE "users" (
   id              UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
   email           VARCHAR(80) NOT NULL,
   password        VARCHAR(80) NOT NULL,
-  tgvmax_number   VARCHAR(11) NOT NULL,
-  created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+  tgvmaxNumber    VARCHAR(11) NOT NULL,
+  createdAt       TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
 );
 
 -- test : insert a row in table "users"
@@ -22,15 +22,19 @@ INSERT INTO users (email, password, tgvmax_number)
 -- create travel status enum
 CREATE TYPE status AS ENUM ('in_progress', 'found', 'not_found');
 
--- create table Travel
+-- create table "travels"
 CREATE TABLE "travels" (
   id              UUID NOT NULL DEFAULT uuid_generate_v4(),
-  user_id         UUID NOT NULL,
+  userId          UUID NOT NULL,
   origin          VARCHAR(80) NOT NULL,
   destination     VARCHAR(80) NOT NULL,
-  from_time       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  to_time         TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  fromTime        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  toTime          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   status          status NOT NULL,
-  last_check      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+  lastCheck       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  createdAt       TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
 );
+
+-- test : insert a row in table "travels"
+INSERT INTO travels (userId, origin, destination, fromTime, toTime, status, lastCheck)
+    VALUES ('678de226-d67b-4902-af8a-933ba2f96da7', 'FRPAR', 'FRNIT', '2019-08-08 00:30:00.987273', '2019-07-30 22:30:00.987273', 'in_progress', '2019-07-31 15:30:00.987273');
