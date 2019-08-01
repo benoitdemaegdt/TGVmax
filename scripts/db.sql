6e46ce65030884b9e78af5b30f519ef6d20c0145
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-08-01 16:09:43 +0200

    change db column from camelCase to snake_case because postgre is case insensitive

--- scripts/db.sql
+++ scripts/db.sql
@@ -9,10 +9,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- create table "users"
 CREATE TABLE "users" (
   id              UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  email           VARCHAR(80) NOT NULL,
+  email           VARCHAR(80) UNIQUE NOT NULL,
   password        VARCHAR(80) NOT NULL,
-  tgvmaxNumber    VARCHAR(11) NOT NULL,
-  createdAt       TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+  tgvmax_number   VARCHAR(11) NOT NULL,
+  created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
 );
 
 -- test : insert a row in table "users"
@@ -25,16 +25,16 @@ CREATE TYPE status AS ENUM ('in_progress', 'found', 'not_found');
 -- create table "travels"
 CREATE TABLE "travels" (
   id              UUID NOT NULL DEFAULT uuid_generate_v4(),
-  userId          UUID NOT NULL,
+  user_id         UUID NOT NULL,
   origin          VARCHAR(80) NOT NULL,
   destination     VARCHAR(80) NOT NULL,
-  fromTime        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  toTime          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  from_time       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  to_time         TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   status          status NOT NULL,
-  lastCheck       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  createdAt       TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+  last_check      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
 );
 
 -- test : insert a row in table "travels"
-INSERT INTO travels (userId, origin, destination, fromTime, toTime, status, lastCheck)
+INSERT INTO travels (user_id, origin, destination, from_time, to_time, status, last_check)
     VALUES ('678de226-d67b-4902-af8a-933ba2f96da7', 'FRPAR', 'FRNIT', '2019-08-08 00:30:00.987273', '2019-07-30 22:30:00.987273', 'in_progress', '2019-07-31 15:30:00.987273');
