6211c14c8398f3203e29fac947b5026bf2e202bf
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-08-06 16:40:12 +0200

    add a database class

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,39 @@
 import * as moment from 'moment-timezone';
-import { Client, QueryResult } from 'pg';
-import Config from './config';
+import Database from './db';
 import { Travel } from './travel';
 import { IAvailability, ITravel } from './types';
-
-/**
- * create postgreSQL client
- */
-const client: Client = new Client({
-  connectionString: Config.dbUrl,
-});
 
 (async(): Promise<void> => {
   /**
    * connect to db
    */
-  await client.connect();
+  await Database.connect();
 
   /**
    * insert one travel and one user into the db
    * Note : this is temporary and will be handled by a proper API later
    */
-  await client.query({
-    text: `INSERT INTO travels(user_id, origin, destination, from_time, to_time, status, last_check)
-    VALUES($1, $2, $3, $4, $5, $6, $7)`,
-    values: [
-      '4e7bcd15-bd14-42bf-8746-bf3e74784b12',
-      'FRPAR',                        // EDIT HERE (find other train stations in ./data/trainStations.json)
-      'FRNIT',                        // EDIT HERE (find other train stations in ./data/trainStations.json)
-      '2019-08-07T00:30:00.987Z',     // EDIT HERE
-      '2019-08-07T22:30:00.987Z',     // EDIT HERE
-      'in_progress',
-      '2019-07-31T15:30:00.987Z',
-    ],
+  await Database.insert('travels', {
+    user_id: '4e7bcd15-bd14-42bf-8746-bf3e74784b12',
+    origin: 'FRPAR',                              // EDIT HERE (find other train stations in ./data/trainStations.json)
+    destination: 'FRNIT',                         // EDIT HERE (find other train stations in ./data/trainStations.json)
+    from_time: '2019-08-07T00:30:00.987Z',        // EDIT HERE
+    to_time: '2019-08-07T22:30:00.987Z',          // EDIT HERE
+    status: 'in_progress',
+    last_check: '2019-07-31T15:30:00.987Z',
   });
 
-  await client.query({
-    text: 'INSERT INTO users(id, email, password, tgvmax_number) VALUES($1, $2, $3, $4)',
-    values: [
-      '4e7bcd15-bd14-42bf-8746-bf3e74784b12',
-      'test@yopmail.com',
-      'my_password',
-      'HC000054321', // EDIT HERE : TGVMAX_NUMBER
-    ],
+  await Database.insert('users', {
+    id: '4e7bcd15-bd14-42bf-8746-bf3e74784b12',
+    email: 'test@yopmail.com',
+    password: 'my_password',
+    tgvmax_number: 'HC000054321',                 // EDIT HERE : TGVMAX_NUMBER
   });
 
   /**
    * fetch all travels "in_progress" from database (only one here)
    */
-  const res: QueryResult = await client.query({
-    text: `SELECT origin, destination, from_time, to_time, tgvmax_number, email FROM travels, users
-    WHERE travels.status = $1 AND travels.user_id = users.id`,
-    values: ['in_progress'],
-  });
-
-  const data: ITravel[] = res.rows as ITravel[];
+  const data: ITravel[] = await Database.findTravel();
 
   /**
    * instantiate the travel
@@ -84,21 +63,21 @@ const client: Client = new Client({
    * Note : this is temporary
    */
   await Promise.all([
-    client.query('DELETE FROM travels'),
-    client.query('DELETE FROM users'),
+    Database.clear('travels'),
+    Database.clear('users'),
   ]);
 
   /**
    * close connection
    */
-  await client.end();
+  await Database.disconnect();
 
 })()
 .catch(async(err: Error) => {
   await Promise.all([
-    client.query('DELETE FROM travels'),
-    client.query('DELETE FROM users'),
+    Database.clear('travels'),
+    Database.clear('users'),
   ]);
-  await client.end();
+  await Database.disconnect();
   console.log(err); // tslint:disable-line
 });
