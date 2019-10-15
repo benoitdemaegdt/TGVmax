0950a63781a6e376e4f39ef20e2c0cd54677c783
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-10-15 11:42:40 +0200

    add PWA support

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import Axios from 'axios';
+import './registerServiceWorker';
 
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem('token');
