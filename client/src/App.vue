a7e48bdd08980552e49db569910e2e5b653858c1
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-11-12 12:26:44 +0100

    change main font

--- client/src/App.vue
+++ client/src/App.vue
@@ -39,3 +39,13 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: auto;
+}
+</style>
